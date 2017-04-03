c6725020fe14835cd2eb170785e9e8f278b02db1
Prajyot Walali <prajyot.walali@coditas.com>
2017-04-03 19:31:27 +0530

    implemented notify and getnotificationbyid

--- execute.py
+++ execute.py
@@ -1,22 +1,57 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import requests
 from time import sleep
 
-count=0
-PIN_NO=37 
-
-def sendAlert():
-    p.pushMessage(CHANNEL_NAME,MESSAGE)
-    return
+count = 0
+PIN_NO = 37
+DEVICE_ID = "MER123"
+SERVER_URL = "http://carsecure.herokuapp.com/"
 
 # GPIO setup ...................................
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN_NO, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(PIN_NO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def block_car(id):
+    # send block status
+    print "BLOCKING"
+    pass
+
+def allow_car(id):
+    # send allow status
+    print "ALLOWING"
+    pass
+
+def waitNact(id):
+    # wait n act accordingly
+    print "WATCHING"
+    for x in range(5):
+        resp = requests.post(SERVER_URL+"api/getnotificationbyid", data={"id": id})
+        if resp.status_code == 200:
+            resp = resp.json()
+            if resp["status"] == "success" and resp["useraction"] != "NONE":
+                if resp["useraction"] == "ALLOW":
+                    allow_car(id)
+                    return
+                elif resp["useraction"] == "BLOCK":
+                    block_car(id)
+                    return
+    block_car(id)
+
+def sendAlert():
+    print "SENDING ALERT"
+    resp = requests.post(SERVER_URL+"api/notify", data={"deviceid": DEVICE_ID}).json()
+    if resp["status"] == "success":
+        print "Notified to user successfully"
+        id = resp["id"]
+        waitNact(id)
+    else:
+        ## Block the car
+        print "Blocking car"
 
 if __name__ == "__main__":
-    print "Hello"
+    sendAlert()
 else:
     while True:
         try:
@@ -25,8 +60,8 @@ else:
             sendAlert()
             sleep(5)
         except (KeyboardInterrupt, SystemExit, Exception):
-        	GPIO.cleanup()
-        	exit()
+            GPIO.cleanup()
+            exit()
 
 
-GPIO.cleanup()
+# GPIO.cleanup()
